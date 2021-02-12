cf611ecc08bbf8525e82af94b4ad8b2aa539e467
manishadas1710 <manishadas1710@gmail.com>
2021-02-12 20:35:21 +0530

    for running locally

--- app.py
+++ app.py
@@ -351,3 +351,6 @@ def editprofile(id):
 def download(filename):
     uploads = 'static/uploads/resumes/'
     return send_from_directory(directory=uploads, filename=filename)
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0',port = 5000)
